42388e5feebea44a293a297525d1199f2d4b15d7
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-13 09:39:29 -0500

    office entity created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppResolver } from './app.resolver';
 import { OfficeModule } from './office/office.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/office',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [],
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
       driver: ApolloDriver,
