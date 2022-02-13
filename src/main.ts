489659e1c5a9cbea2eb7c7eee45869fa725e9b83
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-13 08:22:58 -0500

    app created

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const port = process.env.PORT || 3000;
+  const logger = new Logger();
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(port);
+  logger.log(`App running on port ${port}`);
+}
+bootstrap();
